0f9955b817a21a84776543cb313382d99f15aa1e
dumi33 <dumi3345@gmail.com>
2022-10-07 16:22:21 +0900

    add UX

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,11 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
 
 void main() {
+  WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
+  FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
+  FlutterNativeSplash.remove();
   runApp(MyApp());
 }
 
@@ -136,21 +140,10 @@ class PhotoCheck extends StatefulWidget {
 class _PhotoCheckState extends State<PhotoCheck> {
   @override
   Widget build(BuildContext context) {
-    return Appbar();
-  }
-}
-
-class Appbar extends StatelessWidget {
-  const Appbar({
-    Key? key,
-  }) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
     return DefaultTabController(
-        length: 2,
-        child: Scaffold(
-            appBar: AppBar(
+      length: 2,
+      child: Scaffold(
+        appBar: AppBar(
           title: Image.asset(
             'assets/tmp로고.JPG',
             height: 32,
@@ -166,6 +159,7 @@ class Appbar extends StatelessWidget {
             unselectedLabelColor: Colors.grey,
             labelStyle: TextStyle(
               fontSize: 18,
+              color: Colors.blue,
               fontWeight: FontWeight.bold,
             ),
             tabs: [
@@ -173,6 +167,338 @@ class Appbar extends StatelessWidget {
               Tab(text: "폰트 선택하기"),
             ],
           ),
-        )));
+        ),
+        body: TabBarView(
+          children: [
+            Column(
+              children: [
+                SizedBox(height: 25),
+                Center(
+                    child: Text(
+                  '문장을 따라 작성한 후 촬영하세요',
+                  style: TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                  ),
+                )),
+                SizedBox(height: 25),
+                Container(
+                  height: 250,
+                  width: 250,
+                  decoration: BoxDecoration(
+                    border: Border.all(width: 2, color: Colors.blue),
+                    // borderRadius: BorderRadius.circular(20), //<-- SEE HERE
+                  ),
+                  child: Image.asset(
+                    'assets/모란.JPG',
+                  ),
+                ),
+                SizedBox(height: 25),
+                Column(
+                  children: [
+                    SizedBox(
+                      width: 200,
+                      height: 40,
+                      child: ElevatedButton(
+                        child: Text("재촬영"),
+                        onPressed: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(builder: (_) => (PhotoCheck())),
+                          );
+                        },
+                        style: ElevatedButton.styleFrom(
+                          textStyle: const TextStyle(
+                              fontFamily: 'milasiancirca',
+                              color: Colors.white,
+                              fontSize: 20,
+                              fontStyle: FontStyle.normal),
+                        ),
+                      ),
+                    ),
+                    SizedBox(height: 15),
+                    SizedBox(
+                      width: 200,
+                      height: 40,
+                      child: ElevatedButton(
+                        child: Text("확인"),
+                        onPressed: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(builder: (_) => (SelectFont())),
+                          );
+                        },
+                        style: ElevatedButton.styleFrom(
+                          textStyle: const TextStyle(
+                              fontFamily: 'milasiancirca',
+                              color: Colors.white,
+                              fontSize: 20,
+                              fontStyle: FontStyle.normal),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+
+            /// 나만의 메뉴
+            Center(child: Text("폰트 선택하기")),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class SelectFont extends StatefulWidget {
+  const SelectFont({super.key});
+
+  @override
+  State<SelectFont> createState() => _SelectFontState();
+}
+
+class _SelectFontState extends State<SelectFont> {
+  @override
+  Widget build(BuildContext context) {
+    return DefaultTabController(
+      length: 2,
+      child: Scaffold(
+        appBar: AppBar(
+          title: Image.asset(
+            'assets/tmp로고.JPG',
+            height: 32,
+          ),
+          // centerTitle: true,
+          backgroundColor: Colors.white,
+          elevation: 0.0,
+          centerTitle: true,
+          bottom: TabBar(
+            isScrollable: false,
+            indicatorWeight: 4,
+            labelColor: Colors.black,
+            unselectedLabelColor: Colors.grey,
+            labelStyle: TextStyle(
+              fontSize: 18,
+              color: Colors.blue,
+              fontWeight: FontWeight.bold,
+            ),
+            tabs: [
+              Tab(text: "폰트 제작법"),
+              Tab(text: "폰트 선택하기"),
+            ],
+          ),
+        ),
+        body: TabBarView(
+          children: [
+            Column(
+              children: [
+                SizedBox(height: 25),
+                Center(
+                    child: Text(
+                  '자신만의 폰트를 선택하세요',
+                  style: TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                  ),
+                )),
+                SizedBox(height: 25),
+                Container(
+                  height: 100,
+                  width: 250,
+                  decoration: BoxDecoration(
+                    border: Border.all(width: 2, color: Colors.blue),
+                    // borderRadius: BorderRadius.circular(20), //<-- SEE HERE
+                  ),
+                  child: Image.asset(
+                    'assets/모란.JPG',
+                  ),
+                ),
+                SizedBox(height: 10),
+                Container(
+                  height: 100,
+                  width: 250,
+                  decoration: BoxDecoration(
+                    border: Border.all(width: 2, color: Colors.blue),
+                    // borderRadius: BorderRadius.circular(20), //<-- SEE HERE
+                  ),
+                  child: Image.asset(
+                    'assets/모란1.JPG',
+                  ),
+                ),
+                SizedBox(height: 10),
+                Container(
+                  height: 100,
+                  width: 250,
+                  decoration: BoxDecoration(
+                    border: Border.all(width: 2, color: Colors.blue),
+                    // borderRadius: BorderRadius.circular(20), //<-- SEE HERE
+                  ),
+                  child: Image.asset(
+                    'assets/모란2.JPG',
+                  ),
+                ),
+                SizedBox(height: 25),
+                Column(
+                  children: [
+                    SizedBox(
+                      width: 200,
+                      height: 40,
+                      child: ElevatedButton(
+                        child: Text("재촬영"),
+                        onPressed: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(builder: (_) => (PhotoCheck())),
+                          );
+                        },
+                        style: ElevatedButton.styleFrom(
+                          textStyle: const TextStyle(
+                              fontFamily: 'milasiancirca',
+                              color: Colors.white,
+                              fontSize: 20,
+                              fontStyle: FontStyle.normal),
+                        ),
+                      ),
+                    ),
+                    SizedBox(height: 15),
+                    SizedBox(
+                      width: 200,
+                      height: 40,
+                      child: ElevatedButton(
+                        child: Text("사용하기"),
+                        onPressed: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(builder: (_) => (UseFont())),
+                          );
+                        },
+                        style: ElevatedButton.styleFrom(
+                          textStyle: const TextStyle(
+                              fontFamily: 'milasiancirca',
+                              color: Colors.white,
+                              fontSize: 20,
+                              fontStyle: FontStyle.normal),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+
+            /// 나만의 메뉴
+            Center(child: Text("폰트 선택하기")),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class UseFont extends StatefulWidget {
+  const UseFont({super.key});
+
+  @override
+  State<UseFont> createState() => _UseFontState();
+}
+
+class _UseFontState extends State<UseFont> {
+  @override
+  Widget build(BuildContext context) {
+    return DefaultTabController(
+      length: 2,
+      child: Scaffold(
+        resizeToAvoidBottomInset: false,
+        appBar: AppBar(
+          title: Image.asset(
+            'assets/tmp로고.JPG',
+            height: 32,
+          ),
+          // centerTitle: true,
+          backgroundColor: Colors.white,
+          elevation: 0.0,
+          centerTitle: true,
+          bottom: TabBar(
+            isScrollable: false,
+            indicatorWeight: 4,
+            labelColor: Colors.black,
+            unselectedLabelColor: Colors.grey,
+            labelStyle: TextStyle(
+              fontSize: 18,
+              color: Colors.blue,
+              fontWeight: FontWeight.bold,
+            ),
+            tabs: [
+              Tab(text: "폰트 제작법"),
+              Tab(text: "폰트 선택하기"),
+            ],
+          ),
+        ),
+        body: TabBarView(
+          children: [
+            Column(
+              children: [
+                SizedBox(height: 25),
+                Center(
+                    child: Text(
+                  '자신만의 폰트를 사용해보세요.',
+                  style: TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                  ),
+                )),
+                SizedBox(height: 25),
+                Container(
+                  height: 100,
+                  width: 250,
+                  decoration: BoxDecoration(
+                    border: Border.all(width: 2, color: Colors.blue),
+                    // borderRadius: BorderRadius.circular(20), //<-- SEE HERE
+                  ),
+                  child: Image.asset(
+                    'assets/모란.JPG',
+                  ),
+                ),
+                SizedBox(height: 25),
+                Column(
+                  children: [
+                    SizedBox(
+                        width: 250,
+                        height: 40,
+                        child: TextField(
+                            decoration: InputDecoration(
+                          filled: true,
+                          fillColor: Colors.grey,
+                          border: OutlineInputBorder(),
+                          // labelText: '적용할 글자를 입력하세요',
+                        ))),
+                    SizedBox(height: 15),
+                    SizedBox(
+                      width: 250,
+                      height: 40,
+                      child: ElevatedButton(
+                        child: Text("다운로드"),
+                        onPressed: () {},
+                        style: ElevatedButton.styleFrom(
+                          textStyle: const TextStyle(
+                              fontFamily: 'milasiancirca',
+                              color: Colors.white,
+                              fontSize: 20,
+                              fontStyle: FontStyle.normal),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+
+            /// 나만의 메뉴
+            Center(child: Text("폰트 선택하기")),
+          ],
+        ),
+      ),
+    );
   }
 }
